6e9f18365c87df37874ceb01f87169a5e34156b3
stay-puft-marshmallow-man <72616608+stay-puft-marshmallow-man@users.noreply.github.com>
2021-01-25 10:32:59 -0500

    Update flappy-monster.js

--- flappy-monster.js
+++ flappy-monster.js
@@ -1,12 +1,45 @@
-function FlappyMonster(canvas) {
-  // Base
-  var game = this;
-
-  //Global Attributes
-  game.canvas = canvas;
-  game.context = game.canvas.getContext('2d');
-
-  // Game State
+// Game State
   game.currentState = INITIAL;
 }
+ // Game State
+  switch (game.currentState) {
+    case INITIAL:
+      // DRAW INITIAL SCREEN
+          break;
 
+    // Game State
+  switch (game.currentState) {
+    case INITIAL:
+      // DRAW INITIAL SCREEN
+      game.drawInitialScreen();
+      break;
+
+    case GAME_PLAYING:
+      // DRAW GAME PLAYING SCREEN
+            break;
+    case GAME_OVER:
+      // DRAW GAME OVER SCREEN
+          break;
+  }
+
+  FlappyMonster.prototype.drawInitialScreen = function() {
+  // Base
+  var game = this;
+}
+
+// Start Game
+  window.requestAnimationFrame(function() {
+    game.runGameLoop();
+  });
+
+// Draw
+
+  // Background
+  game.context.fillStyle = 'black';
+  game.context.fillRect(0, 0, game.canvas.width, game.canvas.height);
+
+  // Text
+  game.context.fillStyle = 'white';
+  game.context.font = '36px Arial';
+  game.context.fillText('GAME PLAYING', game.canvas.width / 2 - 100, game.canvas.height / 2);
+};
