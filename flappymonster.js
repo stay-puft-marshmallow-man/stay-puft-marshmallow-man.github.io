9d86932d35140283d288d2ed646283f242fc1b23
stay-puft-marshmallow-man <72616608+stay-puft-marshmallow-man@users.noreply.github.com>
2021-01-27 12:55:24 -0500

    Update flappymonster.js

--- flappymonster.js
+++ flappymonster.js
@@ -1,3 +1,8 @@
+var INITIAL = 1;
+var GAME_PLAYING = 2;
+var GAME_OVER = 3;
+
+
 function FlappyMonster(canvas) {
 // Base
   var game = this;
@@ -8,6 +13,46 @@ function FlappyMonster(canvas) {
 
   // Game State
   game.currentState = INITIAL;
+  
+}
 
+FlappyMonster.prototype.start = function() {
+   // Base
+  var game = this;
+  
+  / Start Game
+  window.requestAnimationFrame(function() {
+    game.runGameLoop();
+  });
+};
+
+FlappyMonster.prototype.start = function() {
+   switch (game.currentState) {
+    case INITIAL:
+      // DRAW INITIAL SCREEN
+      game.drawInitialScreen();
+      break;
+case GAME_PLAYING:
+      // DRAW GAME PLAYING SCREEN
+      game.drawGamePlayingScreen();
+      break;
+case GAME_OVER:
+      // DRAW GAME OVER SCREEN
+      game.drawGameOverScreen();
+      break;
+   }
+};
+FlappyMonster.prototype.drawInitialScreen = function() {
+  // Base
+  var game = this;
+}
+FlappyMonster.prototype.drawGamePlayingScreen = function() {
+  // Base
+  var game = this;
+
+ 
+FlappyMonster.prototype.drawGameOverScreen = function() {
+  // Base
+  var game = this;
 
 }
